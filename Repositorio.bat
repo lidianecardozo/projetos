3b07ea09133f2a7a1179ace6378c0351c26e7747
lidiane.tb <lidiane.tb@gmail.com>
2022-10-25 23:26:11 -0300

    Update ~-2_~-2_2022_2326

--- Repositorio.bat
+++ Repositorio.bat
@@ -1,12 +1,18 @@
 cd c:
 cd C:\Users\lidia\Documents\Archicad
 
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+for /f "usebackq skip=1 tokens=1-3" %%g in (`wmic Path Win32_LocalTime Get Day^,Month^,Year ^| findstr /r /v "^$"`) do (
+  set day=00%%g
+  set month=00%%h
+  set year=%%i
+  )
+rem pad day and month with leading zeros
+set month=%_month:~-2%
+set day=%_day:~-2%
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-echo %mydate%_%mytime%
 
 git add .
 
-git commit -m "Update %mydate%_%mytime%"
+git commit -m "Update %day%_%month%_%year%_%mytime%"
 
 git push origin master
